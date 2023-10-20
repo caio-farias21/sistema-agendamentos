c9721ad94f4977a0ac7710b24d8f8e5cffc1b35b
Acsa Mendes <73358567+acsa962@users.noreply.github.com>
2023-10-20 16:35:23 +0000

    criação de telas

---
+++ arquivo.c
@@ -0,0 +1,11 @@
+#include "telas.h"
+#include <stdio.h>
+
+void teste(){
+    printf("deu certo 2\n");
+}
+
+void main(){
+    telas();
+    teste();
+}
---
+++ telas.h
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+int telas(){
+    printf("deu certo\n");
+}
